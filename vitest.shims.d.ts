5e8c9b59ca48829a8afd99b162631cdc95fe2adc
Salauddin Omar Sifat <mahi@Macintosh-Air.local>
2025-09-24 23:38:35 +0600

    test: add test cases

--- vitest.shims.d.ts
+++ vitest.shims.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="@vitest/browser/providers/playwright" />
+/// <reference types="vitest" />
+/// <reference types="@testing-library/jest-dom" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // add more env variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
