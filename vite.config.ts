d77bfb5284fc6672696c357d0c98250c33a9c0de
mahi160 <omarsifat288@gmail.com>
2025-09-25 22:53:14 +0600

    fix: storybook issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,24 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
-
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "Bizkit UI",
-      fileName: format => `bizkit-ui.${format}.js`
+      fileName: (format) => `bizkit-ui.${format}.js`,
     },
     rolldownOptions: {
       external: ["react", "react-dom"],
       output: {
         globals: {
           react: "React",
-          reactDom: "ReactDOM"
-        }
-      }
-    }
-  },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: 'playwright',
-          instances: [{
-            browser: 'chromium'
-          }]
+          reactDom: "ReactDOM",
         },
-        setupFiles: ['.storybook/vitest.setup.ts']
-      }
-    }]
-  }
+      },
+    },
+  },
 });
