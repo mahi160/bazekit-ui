da8ec72968696e7ba687f691288ea60b6948a430
mahi160 <omarsifat288@gmail.com>
2025-10-09 11:01:10 +0600

    update color

---
+++ src/components/input/Input.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Input } from "./Input";
+
+const meta: Meta<typeof Input> = {
+  title: "Components/Input",
+  component: Input,
+  //   parameters: {
+  //     docs: {
+  //       description: {
+  //         component: `
+  // The **Button** component is a styled wrapper around the native \`<button>\` element.
+  // It uses theme tokens from \`theme.css\` and automatically adapts to light/dark mode.
+  //
+  // ### Features
+  //
+  // - Theming with CSS variables
+  // - Dark mode support (\`data-theme="dark"\`)
+  // - Supports all native button props
+  //   `,
+  //       },
+  //     },
+  //   },
+  //   argTypes: {
+  //     children: {
+  //       control: "text",
+  //       description: "Content inside the button",
+  //     },
+  //     onClick: {
+  //       action: "clicked",
+  //       description: "Click event handler",
+  //     },
+  //   },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {
+  render: () => <Input placeholder="Enter text" />,
+};
