f1a22c672c57433d51f55bc3065a51d6e654c304
Salauddin Omar Sifat <mahi@Macintosh-Air.local>
2025-10-19 19:45:41 +0600

    chore: update name

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { analyzer } from 'vite-bundle-analyzer'
+// import { analyzer } from 'vite-bundle-analyzer'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), analyzer()],
+  plugins: [react()],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
@@ -15,10 +15,10 @@ export default defineConfig({
       external: ['react', 'react-dom', 'react/jsx-runtime', '@base-ui-components/react'],
       output: {
         globals: {
-          react: 'react',
-          reactDom: 'react-dom',
-          reactJsxRuntime: 'react/jsx-runtime',
-          baseUiComponentsReact: '@base-ui-components/react',
+          'react': 'react',
+          'react-dom': 'react-dom',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          '@base-ui-components/react': '@base-ui-components/react',
         },
       },
     },
